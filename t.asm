e7a3b817a81bcb6d8f8b3ac65c37be96f2d7fb43
mAbdullah02-bit <abbi212711@gmail.com>
2024-10-18 17:38:31 +0500

    testing a question for assignment\

--- t.asm
+++ t.asm
@@ -1,49 +1,97 @@
 [org 0x0100]
 jmp start
+arr: dw 5,6,8,l,9,2,8
+size: 6 
+swapper:db 0
 
-sensors: dw 0x8855, 0x0581
-alerts: dw 0,0
-criticalalert: dw 0,0
-warningalert: dw 0,0
-infoalert: dw 0,0
-
-count_ones:
-mov ax,[sensors+bx]
-
-L1:shl ax,1
-jnc skip
-call categorize_alerts
-
-skip:
-dec cx
-cmp cx,0
-jne L1
+swap: 
+push ax
+mov ax,[bx+si]
+xchg ax,[bx+si+2]
+mov [bx+si],ax
+pop ax
 ret
 
+bubblesort:
+push bx
+push cx
+push ax
+push dx
+push si
+mov bx,0
 
-categorize_alerts: add word [alerts+bx],1
-dec cx
-cmp cx,12
-jge critical
-cmp cx,8
-jge warning
-add word [infoalert+bx],1
-jmp return
-warning: add word [warningalert+bx],1
-jmp return
-critical: add word [criticalalert+bx],1
-return: 
-inc cx
+mov cx,[bp-2]
+mov si,[bp+6]
+
+L1:
+mov byte [bp-4],0
+
+L2: mov ax,[si+bx]
+cmp ax,[si+bx+2]
+jbe noswap
+call swap
+mov byte [bp-4],1
+noswap:
+add si,2
+cmp si,cxjne innerloop
+cmp byte [bp-4],1
+jz L1
+pop si
+pop dx 
+pop ax
+pop cx
+pop bx
 ret
 
+statsofarr: 
+push bp
+mov bp,sp
+mov cx,[bp+4]
+shl cx
+push cx
+push swapper
+call bubblesort
+pop di
+push si
+mov si,[bp+6]
+
+
+
+mov ax,[bp+4]
+div 2
+cmp dx ,0
+jne odd
+
+shl ax
+mov bx,ax
+mov ax,[si+bx]
+add ax,[si+bx+2]
+div 2
+mov cx,ax
+
+odd: 
+shl ax
+mov bx
+mov cx,[si+bx]
+
+mov ax,[si]
+mov bx,[si+di]
+
+pop si
+pop bp
+push ax
+push bx
+push cx
+ret
 start:
-xor cx,cx
-xor bx,bx
-mov cx,16
-call count_ones
-add bx,2
-mov cx,16
-call count_ones
+mov bx,arr
+push bx
+mov cx,[size]
+push cx
+call statsofarr
+
+jmp 
+
  mov ax,0x4c00
  int 0x21
 
