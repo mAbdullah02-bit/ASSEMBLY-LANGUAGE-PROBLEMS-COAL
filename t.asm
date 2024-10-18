1600d6ec9ca29df2ff444e17b7758905c627e6a4
mAbdullah02-bit <abbi212711@gmail.com>
2024-10-18 18:46:27 +0500

    Finaol testing

--- t.asm
+++ t.asm
@@ -1,106 +1,112 @@
+
+
 [org 0x0100]
 jmp start
 
-arr: dw 5,6,8,1,9,2,8   
-size: dw 7             
-swapper: dw 0
-
+arr: dw 5,6,8,1,9,2   
+size: dw 6              
+swapper: db 0           
 swap:
     push ax
-    mov ax, [bx+si]
-    xchg ax, [bx+si+2]
-    mov [bx+si], ax
+    mov ax, [si+bx]     
+    mov [si+bx], ax     
     pop ax
-    ret
+    ret 
 
 bubblesort:
-    push bx
-    push cx
-    push ax
-    push dx
+
     push si
-    mov cx, [bp+4]     
-    shl cx, 1         
+    push cx
+    push bx
+    mov cx, [bp-4] 
+    sub cx,2     
+   mov si,[bp+4]      
+       
+L1:
+    mov byte [swapper], 0       
+    mov bx, 0           
 
-outerloop:
-    mov byte [bp-4], 0
-    xor bx, bx       
-    xor si, si
+L2:
+    mov ax, [si+bx]     
+    cmp ax, [si+bx+2]   
+    jbe no_swap         
+    mov byte [swapper], 1      
+    call swap           
 
-innerloop:
+no_swap:
+    add bx, 2          
     cmp bx, cx
-    jge skip_outer     
-    mov ax, [si+bx]
-    cmp ax, [si+bx+2]
-    jbe noswap
-    call swap         
-    mov byte [bp-4], 1 
+    jne L2
+    cmp byte [swapper], 1      
+    jz L1
 
-noswap:
-    add bx, 2
-    cmp bx, cx
-    jl innerloop
 
-skip_outer:
-    cmp byte [bp-4], 1
-    jz outerloop       
-
-    
-    mov ax, [si]       
-    mov bx, [si+cx-2]  
-
-    pop si
-    pop dx
-    pop ax
-    pop cx
     pop bx
-    ret
+    pop cx
+    pop si
+    
+    mov ax, [si]
+  push bx
+    mov bx,cx 
+    sub bx,2     
+    mov dx, [si+bx]
+        
+pop bx
+
+push ax
+push dx
+
+   ret
+
+
 
 statsofarr:
     push bp
     mov bp, sp
-    mov cx, [bp+4]     
-    shl cx, 1        
+    mov cx, [bp+2]       
+    shl cx, 1            
 
-    call bubblesort    
+    call bubblesort     
+come:
+    mov si, [bp+4]       
+    mov ax, [bp+2]      
 
-    mov si, [bp+6]     
-
-   
-    mov ax, [bp+4]
-    test ax, 1         
-    jnz odd            
+    test ax, 1           
+    jnz odd              
 
 even:
-    mov ax, [bp+4]     
-    shr ax, 1          
-    shl ax, 1          
+    mov ax, [bp+2]     
+    shr ax, 1        
+      shl ax,1      
     mov bx, ax
-    mov ax, [si+bx]    
-    add ax, [si+bx+2]  
-    shr ax, 1         
-    mov cx, ax         
+    mov ax, [si+bx]   
+   
+    add ax, [si+bx-2]   
+    shr ax, 1            
+    mov cx, ax           
     jmp done
 
 odd:
-    mov ax, [bp+4]     
-    shr ax, 1          
-    shl ax, 1          
-    mov bx, ax
-    mov cx, [si+bx]    
+    mov ax, [bp+2]      
+    shr ax, 1           
+    shl ax, 1           
+    mov bx, ax           
+    mov cx, [si+bx]     
 
 done:
-    pop bp
+pop bx
+pop ax
+pop bp
     ret
 
-start:
-    mov bx, arr
-    push bx
-    mov cx, [size]
-    push cx
-    call statsofarr
-    pop cx
-    pop bx
 
-    mov ax, 0x4c00
-    int 0x21
+start:
+    mov si, arr          
+    push si
+    mov cx, [size]       
+    push cx
+    call statsofarr     
+  
+
+    mov ax, 0x4c00      
+    int 0x21          
