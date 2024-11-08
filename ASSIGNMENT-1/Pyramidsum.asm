5b17b20ae0ea4c18441ef9eac4ae81dcfefb5b75
mAbdullah02-bit <abbi212711@gmail.com>
2024-11-08 15:40:05 +0500

    fixed a bug

--- ASSIGNMENT-1/Pyramidsum.asm
+++ ASSIGNMENT-1/Pyramidsum.asm
@@ -4,7 +4,8 @@ message: db 'The sum is: $'
 num: dw 6
 sum: dw 0
 
-calcsum: 
+; Triangle implementation
+calcsum1: 
 
 L1:
 mov ax,1
@@ -32,6 +33,28 @@ cmp bx,[num]   ; check no of rows
 jbe L1
 
 ret
+ 
+;Formula implementation
+
+calcsum:
+push ax
+push cx
+push bx
+push dx
+xor cx,cx
+mov bx,1
+Loop1:
+mov bx,ax
+mul bx
+add cx,ax
+inc bx
+cmp bx,[num]
+jne Loop1
+pop dx
+pop bx 
+pop cx
+pop ax
+ret
 
 printsum:
 mov ax,0xb800
@@ -50,15 +73,17 @@ end_string:
 
 mov ax,[sum]
 mov bx,10
+mov cx,0
 L5:     ; Divide the number by 10 to get remainder and print character by character
 xor dx,dx
 div bx
 add dx,0x30
 push dx 
+inc cx
 cmp ax,0
 jne L5
 
-mov cx,4
+
 L7:pop ax
 mov ah,07
 mov [es:di],ax
@@ -90,13 +115,14 @@ nextloc:
 start:
 mov ax,[num]
 add ax,5     ; Exevuting formula
+; inc ax      ; only for 2nd method 
 xchg ax,[num]
 
 mov cx,0
 mov dx,0      ; initializing varables
 mov bx,1
 mov ax,1
-call calcsum   ; calling functions
+call calcsum1   ; calling functions
 call clrscr   ;clear the screen
 call printsum   ; show output
 mov ax,[sum]
